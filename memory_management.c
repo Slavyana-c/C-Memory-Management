9c57f9d9082a040527143ee8d70860db7f14d1e6
Slavyana Chervenkondeva <sc17sdc@leeds.ac.uk>
2018-11-08 21:09:07 +0000

    All functions without the alignment

--- memory_management.c
+++ memory_management.c
@@ -1,4 +1,3 @@
-#include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <stddef.h>
@@ -21,21 +20,10 @@ typedef struct block {
 Block *head = NULL;
 Block *tail = NULL;
 
-//void requestTest() {
-//	printf("Start: %d \n", sbrk(0));
-//	// Why Print random address???
-//	Block *newBlock = sbrk(1);
-//	Block *firstBlock = requestMemory(3000);
-//	Block *secondBlock = requestMemory(5000);
-//	Block *thirdBlock = requestMemory(1000);
-//
-//	printf("Address 1: %d, Address 2: %d, Address 3: %d", firstBlock, secondBlock, thirdBlock);
-//}
-
-// Ret a new block of memory
+// Requests memory and returns a new block
 Block *requestMemory(size_t size) {
 	Block *newBlock = (Block*) sbrk(0);
-	printf("Heap top: %d \n", newBlock);
+	printf("Heap top: %ld \n", (long) newBlock);
 
 	// Request memory of size that is a multiple of 4096
 	size_t requestSize = ceil(
@@ -44,7 +32,7 @@ Block *requestMemory(size_t size) {
 
 	printf("Size: %ld, Block: %ld\n", size, sizeof(Block));
 	printf("Requesting %ld memory\n", requestSize);
-	printf("New heap top: %d \n", sbrk(0));
+	printf("New heap top: %ld \n", (long) sbrk(0));
 
 	// Command failure
 	if (request == (void*) -1) {
@@ -65,28 +53,39 @@ Block *requestMemory(size_t size) {
 	return newBlock;
 }
 
+int getPadding(Block *block) {
+	int padding = 1;
+	if ((long) block % 8 != 0) {
+		padding = 8 - ((long) block % 8);
+	}
+
+	return padding;
+}
+
 // Returns a free block with enough memory, if any
-// Using the "Best Fit" technique
 Block *findFreeBlock(size_t size) {
 	Block *currentBlock = head;
 	Block *bestBlock = NULL;
 	int sizeDifference = INT_MAX;
 	int minDifference = INT_MAX;
+	int padding = 0;
 
 	while (currentBlock != NULL) {
-		if (!currentBlock->free || currentBlock->size < size) {
-			tail = currentBlock;
 
+		padding = getPadding(currentBlock);
+		printf("Expected padding - %d\n", padding);
+		if (!currentBlock->free || currentBlock->size + padding < size) {
 			printf("Block checked, not free\n");
 		} else {
-			printf("Found free block %d of size %d\n",currentBlock, currentBlock->size);
+			printf("Found free block %ld of size %ld\n", (long) currentBlock,
+					(long) currentBlock->size);
 			sizeDifference = currentBlock->size - size;
 			if (sizeDifference < minDifference) {
-				printf("New best block!- min diff: %d\n", sizeDifference);
+				printf("New best block!- minimum difference: %d\n",
+						sizeDifference);
 				minDifference = sizeDifference;
 				bestBlock = currentBlock;
 			}
-
 		}
 		currentBlock = currentBlock->next;
 	}
@@ -94,7 +93,10 @@ Block *findFreeBlock(size_t size) {
 	return bestBlock;
 }
 
+// Splits block if possible
 void split(Block *bigBlock, size_t size) {
+	//int padding = getPadding(bigBlock);
+
 	Block *new = (void*) ((void*) bigBlock + size + sizeof(Block));
 	new->size = (bigBlock->size) - size - sizeof(Block);
 	new->free = 1;
@@ -109,7 +111,7 @@ void split(Block *bigBlock, size_t size) {
 			new->size);
 }
 
-// Returns
+// Returns a pointer to the free space
 void * _malloc(size_t size) {
 	Block *block;
 
@@ -138,6 +140,7 @@ void * _malloc(size_t size) {
 
 	// Found free block
 	//printf("Found free block!\n");
+	tail = block;
 	if (block->size - size > sizeof(Block)) {
 		printf("Needs splitting\n");
 		split(block, size);
@@ -145,109 +148,109 @@ void * _malloc(size_t size) {
 	} else {
 		block->free = 0;
 		//printf("Formula: %d\n", block->size - size - sizeof(Block));
-		if((int)(block->size - size - sizeof(Block)) < 0) {
+		if ((int) (block->size - size - sizeof(Block)) < 0) {
 			printf("Not perfect size, but can't be split\n");
-		}
-		else {
+		} else {
 			printf("Perfect size\n");
 		}
 
 	}
-	printf("Block: %d, %d, Returning %d\n", block, sizeof(Block), (Block*)((void *)block + 5));
-	return (Block*)((void*)block + sizeof(Block));
+	return ((Block*) ((void*) block + sizeof(Block)));
 }
 
+// Merges blocks when possible
 Block* merge(Block *freeBlock) {
 
 	Block *prevBlock = freeBlock->prev;
 	Block *nextBlock = freeBlock->next;
 
-
 	if (prevBlock != NULL && prevBlock->free) {
-		printf("Previous block is free, size -> %d\n", prevBlock->size);
+		printf("Previous block is free, size -> %zu\n", prevBlock->size);
 		prevBlock->size += freeBlock->size + sizeof(Block);
 		prevBlock->next = freeBlock->next;
 		freeBlock = prevBlock;
 		nextBlock = freeBlock->next;
-
 	}
 
 	if (nextBlock != NULL && nextBlock->free) {
-		printf("Next block is free, size -> %d\n", nextBlock->size);
+		printf("Next block is free, size -> %zu\n", nextBlock->size);
 		freeBlock->size += nextBlock->size + sizeof(Block);
 		freeBlock->next = nextBlock->next;
 	}
 
 	printf("Successfully merged\n");
-	printf("New block of size %ld\n", freeBlock->size);
+	printf("New block of size %zu\n", freeBlock->size);
 
 	return freeBlock;
-
 }
 
+// Returns memory back to the OS
 void returnMemory(Block* block) {
 
-	void *endOfBlock = (void*)block + sizeof(Block) + block->size;
-	printf("Block ends at %d, top is %d\n", endOfBlock, sbrk(0));
+	void *endOfBlock = (void*) block + sizeof(Block) + block->size;
+	printf("Block ends at %ld, top is %ld\n", (long) endOfBlock,
+			(long) sbrk(0));
 
-	if(endOfBlock == sbrk(0)) {
-		if(tail == block) {
-			tail = block -> prev;
+	if (endOfBlock == sbrk(0)) {
+		if (tail == block) {
+			tail = block->prev;
 			printf("This block was tail\n");
 		}
-		if(head == block) {
-			head = block-> next;
+		if (head == block) {
+			head = block->next;
 			printf("This block was head\n");
 		}
 
 		size_t requestSize = -1 * (block->size + sizeof(Block));
-		printf("Requesting: %d\n", requestSize );
+		printf("Requesting: %zu\n", requestSize);
 		void *request = (Block *) sbrk(requestSize);
-//			// Command failure
+		// Command failure
 		if (request == (void*) -1) {
-				printf("Request failed\n");
-				return;
+			printf("Request failed\n");
+			return;
 		} else {
-			printf("new %d\n", sbrk(0));
+			printf("new %ld\n", (long) sbrk(0));
 		}
 	}
-
 }
 
+// Frees the block of memory
 void _free(void * ptr) {
 
-	printf("%d\n", ptr);
-	Block* block = (Block*)(ptr - sizeof(Block));
+	printf("%ld\n", (long) ptr);
+	Block* block = (Block*) (ptr - sizeof(Block));
 	block->free = 1;
-	printf("Freed %d, size %d\n", block, block->size);
-
-	//if((block->prev != NULL && block->prev->free )|| (block->next != NULL && block->prev->free ))
+	printf("Freed %ld, size %zu\n", (long) block, block->size);
 	Block *mergedBlock = merge(block);
 
 	returnMemory(mergedBlock);
+}
 
+// Prints List
+void printList() {
+	Block *current = head;
+	int i = 1;
+	while (current != NULL) {
+		printf("Block%d: %ld -> size: %zu \n", i, (long) current,
+				current->size);
+		current = current->next;
+		i++;
+	}
 }
 
 int main() {
 
-	//void *init = sbrk(1);5
-	Block *Block1 = _malloc(4037);
+	int sum = 10;
+
+	while (sum <= 50) {
+		//Block *Block1 = _malloc(sum);
+		sum += 5;
+	}
+
+	printList();
+//	Block *Block1 = _malloc(4037);
 //	printf("--------------\n");
-//	Block *Block2 = _malloc(1);
-//	printf("--------------\n");
-//	Block *Block3 = _malloc(4);
-//	printf("--------------\n");
-
-	//printf("%d %d %d\n", Block1, Block2, Block3);
-	_free(Block1);
-//	_free(Block3);
-//	_free(Block2);
-
-	printf("--------------\n");
-
-
-
-	Block *Block4 = _malloc(10);
+//	Block *Block4 = _malloc(4);
 
 	return 0;
 }
